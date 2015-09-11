f00884ccd42d7beb6d37d37e7150369768ce9cda
KarmaAK <Karma-AK@users.noreply.github.com>
2015-09-11 13:47:25 +0800

    Update Init.pp

--- Init.pp
+++ Init.pp
@@ -1,21 +1,19 @@
 class testing {
 
-file { "zen.txt":
-ensure => file,
-owner => 'root',
-group => 'root',
-mode => '0755',
+  file { "zen.txt":     #set the full path or the file name here
+      ensure => file,
+      owner => 'root',
+      group => 'root',
+      mode => '0755',
+      source => "/etc/puppetlabs/puppet/modules/testing/zen.txt",
+      path => "/var/tmp/zen.txt",
+      require => Exec["check_dir"],
+      audit =>'all' ,    #set audit here so that you can see changes if any during a puppet run. Feel free to skip this.
+ }
 
-source => "/etc/puppetlabs/puppet/modules/testing/zen.txt",
-
-path => "/var/tmp/zen.txt",
-require => Exec["check_dir"],
-audit =>'all' ,
-}
-
-exec { "check_dir":
-command => '/bin/true',
-onlyif => '/usr/bin/test -d  /var/tmp/',
-}
+  exec { "check_dir":
+      command => '/bin/true',
+      onlyif => '/usr/bin/test -d  /var/tmp/',
+ }
 
 }
